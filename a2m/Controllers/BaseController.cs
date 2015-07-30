b77dc7ba7d703ca1be6dd09197c59926b1ee45c2
FernandoBeltran <fernando.beltran@gmail.com>
2015-07-30 21:02:35 +0200

    Compress GZIP

--- a2m/Controllers/BaseController.cs
+++ a2m/Controllers/BaseController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,41 @@ namespace a2m.Controllers
     /// </summary>
     public class BaseController : Controller
     {
+        /// <summary>
+        /// Añade compresión GZIP a las vistas
+        /// </summary>
+        /// <example> Usage:
+        ///             [Compress] 
+        ///             public class BookingController : BaseController
+        ///             {...}	
+        ///
+        ///             [CompressFilter]
+        ///             public void Category(string name, int? page)
+        /// </example>
+        public class CompressAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+
+                var encodingsAccepted = filterContext.HttpContext.Request.Headers["Accept-Encoding"];
+                if (string.IsNullOrEmpty(encodingsAccepted)) return;
+
+                encodingsAccepted = encodingsAccepted.ToLowerInvariant();
+                var response = filterContext.HttpContext.Response;
+
+                if (encodingsAccepted.Contains("deflate"))
+                {
+                    response.AppendHeader("Content-encoding", "deflate");
+                    response.Filter = new DeflateStream(response.Filter, CompressionMode.Compress);
+                }
+                else if (encodingsAccepted.Contains("gzip"))
+                {
+                    response.AppendHeader("Content-encoding", "gzip");
+                    response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
+                }
+            }
+        }
+
         /// <summary>
         /// Añade el atributo No-CACHE a la vista
         /// </summary>

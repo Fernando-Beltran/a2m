04db54b6f07f8307882aa142aa74fc9c7db39ffb
GabiDesign <gabidegabriel@hotmail.com>
2015-08-31 19:18:20 +0200

    Listado con un footer provisional y con todas las funcionabilidades

--- a2m/Content/a2m/js/events.js
+++ a2m/Content/a2m/js/events.js
@@ -1,18 +1,29 @@
-$(document).ready(function() {
-    $("#link-down-menu").click(function(){
+$(document).ready(function () {
+    $("#link-down-menu").click(function () {
         console.log("botón menú clickado")
         $(".menu ul").slideToggle("easing");
     });
-    $("#link-down-buy").click(function(){
+    $("#link-down-buy").click(function () {
         console.log("botón tus pedidos clickado")
         $("#form-acces-user").slideToggle("fast");
     });
-    $("#link-down-bussines").click(function(){
+    $("#link-down-bussines").click(function () {
         console.log("botón tus pedidos clickado")
         $("#form-acces-bussines").slideToggle("fast");
     });
-    $(".close-x").click(function(){
+    $(".close-x").click(function () {
         $("#form-acces-bussines").slideUp("fast");
         $("#form-acces-user").slideUp("fast");
+    });
+    //Desplegable filtros
+    $(window).resize(function () {
+        var viewportWidth = $(window).width();
+        if (viewportWidth <= 850) {
+            $("#btnFilter").click(function () {
+                console.log("botón menú clickado")
+                $(".content-filter").slideToggle("easing");
+                bind();
+            });
+        }
     });
 });
